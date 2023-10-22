e60bbf9728dde5271460d60cdcd83c8a93a15372
Cahyo Permono <cahyo.adi@imaniprima.com>
2023-10-22 19:22:37 +0700

    Format Imports

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,8 +1,7 @@
 import { Table, TableColumnHeaderCell } from "@radix-ui/themes";
 import React from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 import IssueAction from "./IssueAction";
+import { Skeleton } from "@/app/components";
 
 const LoadingIssuePage = () => {
   const issues = [1, 2, 3, 4, 5];
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,8 +9,7 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+import { ErrorMessage, Spinner } from "@/app/components";
 import dynamic from "next/dynamic";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
