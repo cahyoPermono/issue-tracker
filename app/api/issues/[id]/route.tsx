ab9b2504aee3151b93f8384ee56b3781d6bed682
Cahyo Permono <cahyo.adi@imaniprima.com>
2023-10-24 07:56:09 +0700

    Handle Error

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -35,3 +35,29 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+  }
+
+  console.log('masuk');
+  
+
+  await prisma.issue.delete({
+    where: {
+      id: issue.id,
+    },
+  });
+
+  return NextResponse.json({});
+}
