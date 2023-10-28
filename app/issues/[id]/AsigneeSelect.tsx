fab55e7feeb1c265b122c3011abd448c2c2699c6
Cahyo Permono <cahyo.adi@imaniprima.com>
2023-10-28 19:01:46 +0700

    Refactor Assigne Select

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -5,29 +5,22 @@ import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 const AsigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ["asignee"],
-    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, //60s
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton height="2rem" />;
 
   if (error) return null;
 
+  const assignedTo = (userId: string) => {
+    axios.patch("/api/issues/" + issue.id, {
+      assignedTo: userId === "0" ? null : userId,
+    });
+  };
+
   return (
     <Select.Root
       defaultValue={issue.assignedTo || "0"}
-      onValueChange={(userId) => {
-        axios.patch("/api/issues/" + issue.id, {
-          assignedTo: userId === "0" ? null : userId,
-        });
-      }}
+      onValueChange={assignedTo}
     >
       <Select.Trigger placeholder="Asign to.." />
       <Select.Content>
@@ -44,5 +37,13 @@ const AsigneeSelect = ({ issue }: { issue: Issue }) => {
     </Select.Root>
   );
 };
+
+const useUsers = () =>
+  useQuery({
+    queryKey: ["asignee"],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3,
+  });
 
 export default AsigneeSelect;
